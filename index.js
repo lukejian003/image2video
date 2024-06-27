5536d837c252722c015f01bb0ba51e07b083c486
lkj <1104337508@qq.com>
2024-06-27 21:42:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,199 @@
+const fs = require('fs').promises;
+const path = require('path');
+const readline = require('readline');
+const ffmpeg = require('fluent-ffmpeg');
+const sizeOf = require('image-size');
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const ProgressBar = require('./progress_bar.js');
+const config = require('./config.json');
+
+// 确保输出文件夹存在，如果不存在就自动创建
+async function ensureDirectoryExists(directory) {
+  try {
+    await fs.access(directory);
+  } catch (error) {
+    console.error(`${directory} 文件夹不存在，正在创建该文件夹...`);
+    try {
+      await fs.mkdir(directory, { recursive: true });
+      console.log(`${directory} 文件夹创建成功。`);
+    } catch (err) {
+      console.error(`无法创建 ${directory} 文件夹: ${err.message}`);
+      throw err;
+    }
+  }
+}
+
+
+// 确保输出文件夹存在
+async function ensureOutputDirectoriesExist() {
+  try {
+    await ensureDirectoryExists(config.videoDir);
+    await ensureDirectoryExists(config.imageDir);
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+// 确保音频文件夹存在
+async function ensureAudioDirectoryExists() {
+  try {
+    await ensureDirectoryExists(config.audioDir);
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+// 检测并创建错误日志文件
+async function ensureErrorLogFileExists() {
+  const logFilePath = path.join(process.cwd(), 'error.log'); // 使用当前工作目录
+  try {
+    await fs.access(logFilePath);
+  } catch (error) {
+    // console.error(`错误日志文件不存在，正在创建: ${logFilePath}`);
+    try {
+      await fs.writeFile(logFilePath, '');
+      // console.log('错误日志文件创建成功。');
+    } catch (err) {
+      console.error(`无法创建错误日志文件: ${err.message}`);
+      process.exit(1);
+    }
+  }
+}
+
+// 获取音频文件列表
+async function getAudioFiles() {
+  try {
+    return await fs.readdir(config.audioDir);
+  } catch (error) {
+    console.error(`无法读取音频文件: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+// 记录错误日志
+async function logError(error) {
+  const logFilePath = path.join(process.cwd(), 'error.log'); // 使用当前工作目录
+  const errorMessage = `${new Date().toISOString()}: ${error.message}\n`;
+  try {
+    await fs.appendFile(logFilePath, errorMessage);
+    console.error('发生错误，请查看 error.log 文件获取详细信息。');
+  } catch (err) {
+    console.error(`无法记录错误日志: ${err.message}`);
+  }
+}
+
+// 入口函数
+async function main() {
+  try {
+    await ensureErrorLogFileExists();
+    await ensureOutputDirectoriesExist();
+    await ensureAudioDirectoryExists();
+    const audioFiles = await getAudioFiles();
+
+    for (let i = 0; i < audioFiles.length; i++) {
+      const audioFile = audioFiles[i];
+      const audioPath = path.join(config.audioDir, audioFile);
+      const audioFileName = path.basename(audioFile, path.extname(audioFile));
+      const videoPath = path.join(config.videoDir, `${audioFileName}.mp4`);
+      const imagePath = path.join(config.imageDir, `${audioFileName}.jpg`);
+      const pb = new ProgressBar(videoPath, 0);
+      // 获取图片尺寸
+      const { width, height } = sizeOf(config.imageFile);
+
+      // 生成带有文字的新图片
+      const watermarkBuffer = await generateWatermark(audioFileName, config.watermarkFontPath, config.watermarkFontSize, width, height);
+
+      // 将文字图片与原始图片合成
+      const finalImageBuffer = await compositeImages(config.imageFile, watermarkBuffer, width, height);
+      await fs.writeFile(imagePath, finalImageBuffer);
+
+      // 使用 ffmpeg 生成视频
+      await generateVideo(audioPath, imagePath, videoPath, width, height, pb);
+    }
+
+    console.log('处理完成，请按下回车键退出。');
+    // 等待用户按下回车键
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('按下回车键退出。', () => {
+      rl.close();
+      process.exit(0);
+    });
+  } catch (error) {
+    await logError(error);
+    console.log('发生错误，请按下回车键退出。');
+    // 等待用户按下回车键
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('按下回车键退出。', () => {
+      rl.close();
+      process.exit(1);
+    });
+  }
+}
+
+async function generateWatermark(text, fontPath, fontSize, width, height) {
+  registerFont(fontPath, { family: 'custom-font' });
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+  ctx.font = `${fontSize}px "custom-font"`;
+  ctx.fillStyle = 'white';
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.fillText(text, width / 2, height / 2);
+  return canvas.toBuffer();
+}
+
+async function compositeImages(backgroundImagePath, watermarkBuffer, width, height) {
+  const backgroundImage = await loadImage(backgroundImagePath);
+  const watermarkImage = await loadImage(watermarkBuffer);
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+  // 绘制背景图片
+  ctx.drawImage(backgroundImage, 0, 0, width, height);
+  // 绘制文字图片
+  ctx.drawImage(watermarkImage, 0, 0, width, height);
+  return canvas.toBuffer();
+}
+
+async function generateVideo(audioPath, imagePath, videoPath, width, height, pb) {
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .input(audioPath)
+      .input(imagePath)
+      .videoCodec('libx264')
+      .size(`${width}x${height}`)
+      .outputOptions(['-pix_fmt yuv420p', '-crf 24', '-preset 8', '-r 6', '-b:v 6M', '-x264-params', 'keyint=infinite:scenecut=60', '-vf', 'scale=768:432:flags=lanczos'])
+      .save(videoPath)
+      .on('progress', (progress) => {
+        const percent = progress.percent.toFixed(2);
+        pb.render({ completed: percent, total: 100 });
+      })
+      .on('end', () => {
+        pb.render({ completed: 100, total: 100 });
+        console.log(`\n视频 ${videoPath} 已生成`);
+        resolve();
+      })
+      .on('error', (err) => {
+        reject(new Error(`视频生成错误: ${err.message}`));
+      });
+  });
+}
+
+main().catch(async (error) => {
+  await logError(error);
+  console.log('发生错误，请按下回车键退出。');
+  // 等待用户按下回车键
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('按下回车键退出。', () => {
+    rl.close();
+    process.exit(1);
+  });
+});
